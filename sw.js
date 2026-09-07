2fdf7b464cbcd2972ee04b7aff61fe6447d29c33
manucaama93 <manucaama93@gmail.com>
2026-09-07 18:53:58 +0200

    sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Service Worker básico para activar el modo PWA/Standalone en Android
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
+});
